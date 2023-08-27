42faad9875ae17f75386672283ca404444702183
badrule <badrul.miteh@gmail.com>
2023-08-27 02:22:29 +0000

    this is second project that I made

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,33 +10,88 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // Application name
-      title: 'Flutter Hello World',
+      title: 'Test Flutter Layout',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    var s = 'Hello Flutter';
+    var widget = Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(0.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  BlueBox(),
+                  Text('XXXXX'),
+                ],
+              )),
+            ),
+            Expanded(
+              flex: 1,
+              child: Center(
+                  child: ElevatedButton(
+                onPressed: () {},
+                child: Text('START'),
+              )),
+            ),
+          ],
         ),
+      ),
+    );
+
+    return widget;
+  }
+}
+
+class BlueBox extends StatelessWidget {
+  const BlueBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      width: 50.0,
+      height: 50.0,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(),
+      ),
+    );
+  }
+}
+
+class BlueBox100 extends StatelessWidget {
+  const BlueBox100({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+        color: Colors.pink,
+        border: Border.all(),
       ),
     );
   }
